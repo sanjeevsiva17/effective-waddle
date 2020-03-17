e9e3962ca5ab661b0c35625cbfa942cd581a0dc3
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-03-17 17:05:13 +0530

    solved ex-7

--- ex-7.py
+++ ex-7.py
@@ -6,3 +6,20 @@
 # Output should be a dictionary key as the random integer and value is the frequency
 # Eg: random_integers_list = [1,2,3,4,1,3,6]
 # outputs: frequencies = {1:2, 2:1, 3:2, 4: 1, 6: 1}
+
+from random import randint
+
+random_integers = []
+number_freq = {}
+for i in range(100):
+    random_integers.append(randint(0,100))
+
+
+for i in random_integers:
+    if i in number_freq:
+        number_freq[i] +=1
+    else:
+        number_freq[i] = 1
+print("random integers list = ", random_integers)
+print("frequencies = ", number_freq)
+    
