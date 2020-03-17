6d548367e1eb981ff613b907b62a1fdbfdf027e0
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-03-17 16:06:50 +0530

    solved ex-4

--- ex-4.py
+++ ex-4.py
@@ -1,3 +1,11 @@
 # Ex-5
 # Extract filenames that has txt as extension
 filenames = ['file1.txt', 'file2.txt', 'file3.pdf', 'file4.pdf']
+
+txtfiles = []
+
+for f in filenames:
+    if 'txt' in f:
+        txtfiles.append(f)
+
+print(txtfiles)
