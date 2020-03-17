6431094e93a3148250b6c54d98658d051632ae51
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-03-17 16:13:40 +0530

    finished ex-6

--- ex-6.py
+++ ex-6.py
@@ -2,7 +2,10 @@
 # Write a function which takes a number as argument.
 # Print all odd numbers till that number.
 
-
+def print_odd_numbers(n):
+    for i in range(n):
+        if i%2 != 0:
+            print(i)
 
 print_odd_numbers(20)
 
