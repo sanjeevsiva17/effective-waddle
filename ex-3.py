d9d6f5a7bd309fc47a9e7053f78cdbcf5a047f54
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-03-17 16:03:57 +0530

    solved ex-3

--- ex-3.py
+++ ex-3.py
@@ -3,3 +3,9 @@
 # Take a number as input from user and print twice the number
 # eg: print_twice(2)
 # Output: 4
+
+def print_twice(number):
+    return 2*number
+
+n = input()
+print(print_twice(int(n)))
