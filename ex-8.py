331f83e43d08dbbf36b5602562af281f2a72fd6b
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-03-17 17:22:16 +0530

    solved ex-8

--- ex-8.py
+++ ex-8.py
@@ -2,3 +2,17 @@
 # Write a function isPrime, that takes a number as argument.
 # Returns true if it is a prime number, false if it is not.
 # Eg: is_prime(20) -> False, is_prime(13) -> True
+
+def isPrime(num):
+    if num > 1:             
+        for i in range(2, num//2): 
+            if (num % i) == 0: 
+                return False 
+        else: 
+            return True
+
+    else: 
+        return False
+
+print(isPrime(20))
+print(isPrime(13))
