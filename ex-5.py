cf774a93f4f2a087cef57d2d01576df026657692
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-03-17 16:11:51 +0530

    solved ex-5

--- ex-5.py
+++ ex-5.py
@@ -2,3 +2,11 @@
 # Read about the module random, it has method randint().
 # https://docs.python.org/2/library/random.html
 # Create a list with 100 random integers.
+
+from random import randint
+
+random_integers = []
+for i in range(100):
+    random_integers.append(randint(0,100))
+
+print(random_integers)
