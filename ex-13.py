dc29ec72ec8f5bf22d2e3c35b72c56e1985385c2
sanjeevsiva17 <sanjeevsiva17@gmail.com>
2020-03-18 17:52:20 +0530

    Finished ex-13

--- ex-13.py
+++ ex-13.py
@@ -4,3 +4,24 @@
 # Python has beautifulsoup library to parse html.
 # http://www.crummy.com/software/BeautifulSoup/bs4/doc/
 # Print all anchor tags, exclude relative links
+
+
+import urllib2
+import re
+
+from bs4 import BeautifulSoup
+
+
+try:
+    response = urllib2.urlopen('http://www.mattmakai.com/links-learning-web-fundamentals.html')
+    html = response.read()
+
+except Exception as e:
+    print(e)
+
+soup = BeautifulSoup(html, 'html.parser')
+exp = re.compile("^http")
+
+for a in soup.find_all('a', href=True):
+    if exp.match(a['href']):
+        print(a['href'])
